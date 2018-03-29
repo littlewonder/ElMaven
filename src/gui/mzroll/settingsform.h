23149ee00c989dbfc000687d9d8edfa584894a90
Kushal <kushal.gupta@elucidata.io>
2018-03-29 14:01:56 +0530

    define startMsconvert function in SettingsForm class.

--- src/gui/mzroll/settingsform.h
+++ src/gui/mzroll/settingsform.h
@@ -34,6 +34,8 @@ class SettingsForm : public QDialog, public Ui_SettingsForm
             void setGroupRankStatus();
             void setInitialGroupRank();
             void dialogRejected();
+            void setIsotopicPeakFiltering();
+            void startMsconvert();
 
             /*
             * [Enable/disable deltaRtWeight slider and display appropriate group rank formula in UI]
